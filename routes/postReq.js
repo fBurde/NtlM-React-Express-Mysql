3fda3ec433e64463ace16d36e404328652d658c3
fburde <Burde@nbs-hamburg.de>
2020-10-26 12:09:18 +0100

    client still not up

--- routes/postReq.js
+++ routes/postReq.js
@@ -36,6 +36,7 @@ router.post('/register', async (req,res)=>{
 router.post('/login', async (req,res)=>{
     //todo add data authentication
               //Validate User
+              console.log("INSIDE LOGIN ",req.body);
               const {error} = loginValidation(req.body)
               if(error) return res.status(400).send(error.details[0].message)
               
